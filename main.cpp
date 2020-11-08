90deca2b9f9f44098a0b2358716297883125b010
Ben Worthington <benworthin@icloud.com>
2020-11-07 23:33:30 -0600

    Restructured program to take advantage of read_index(). Finished store() function

--- main.cpp
+++ main.cpp
@@ -27,6 +27,12 @@ void read_index(int &row, int &col)
         cout << "Error: invalid row\n";
 }
 
+void store(float data[ROWS][COLS], int row, int col, float value) {
+
+    data[row][col] = value;
+
+}
+
 void print(float data[ROWS][COLS], int r1, int c1, int r2, int c2)
 {
     for (int row = r1; row <= r2; row++)
@@ -37,134 +43,85 @@ void print(float data[ROWS][COLS], int r1, int c1, int r2, int c2)
     }
 }
 
-void getUserInput(string &userInput) {
+void getUserInput(string &userCommandInput) {
 
-    cout << "Enter command" << endl;
-    cout << ">> ";
-    getline(cin, userInput);
+    cout << "Enter command >> ";
+    cin >> userCommandInput;
 
-    transform(userInput.begin(), userInput.end(), userInput.begin(), ::toupper);
-
-}
-
-void parseUserInput(string &userInput, string commandParameters[]) {
-
-    char* charArray;
-    charArray = &userInput[0];
-
-    char* parameters = strtok(charArray, " ");
-
-    int i = 0;
-    while (parameters != NULL) {
-
-        commandParameters[i] = parameters;
-
-        parameters = strtok(NULL, " ");
-
-        i++;
-
-    }
+    transform(userCommandInput.begin(), userCommandInput.end(), userCommandInput.begin(), ::toupper);
 
 }
 
-void validateUserCommand(string commandParameters[], string validCommands[], string &userInput) {
+void validateUserCommand(string &userCommandInput, string validCommands[], float data[ROWS][COLS], bool &keepGoing) {
 
     bool isValidCommand = false;
     int commandIndex = 0;
+    int row, col = 0;
+    float value = 0;
 
-    while (!isValidCommand) {
+    for (int i = 0; i < 10; i++) {
 
-        for (int i = 0; i < 10; i++) {
+        if (userCommandInput == validCommands[i]) {
 
-            if (commandParameters[0] == validCommands[i]) {
+            commandIndex = i;
 
-                commandIndex = i;
+            switch (commandIndex) {
 
-                switch (commandIndex) {
+                case 0:
+                    cin >> value;
+                    read_index(row, col);
+                    store(data, row, col, value);
 
-                    case 0:
-                        //STORE command
-//                        store();
+                    break;
+                case 1:
+                    //RANDOMIZE command
+                    //randomize();
 
-                        isValidCommand = true;
+                    break;
+                case 2:
+                    //MIN command
+                    //min();
 
-                        break;
-                    case 1:
-                        //RANDOMIZE command
-                        //randomize();
+                    break;
+                case 3:
+                    //MAX command
+                    //max();
 
-                        isValidCommand = true;
+                    break;
+                case 4:
+                    //SUM command
+                    //sum();
 
-                        break;
-                    case 2:
-                        //MIN command
-                        //min();
+                    break;
+                case 5:
+                    //AVE command
+                    //average();
 
-                        isValidCommand = true;
+                    break;
+                case 6:
+                    //VAR command
+                    //variance();
 
-                        break;
-                    case 3:
-                        //MAX command
-                        //max();
+                    break;
+                case 7:
+                    //STD command
+                    //standardDeviation();
 
-                        isValidCommand = true;
+                    break;
+                case 8:
+                    //PRINT command
+                    //print();
 
-                        break;
-                    case 4:
-                        //SUM command
-                        //sum();
+                    break;
+                case 9:
+                    //QUIT command
+                    keepGoing = false;
 
-                        isValidCommand = true;
-
-                        break;
-                    case 5:
-                        //AVE command
-                        //average();
-
-                        isValidCommand = true;
-
-                        break;
-                    case 6:
-                        //VAR command
-                        //variance();
-
-                        isValidCommand = true;
-
-                        break;
-                    case 7:
-                        //STD command
-                        //standardDeviation();
-
-                        isValidCommand = true;
-
-                        break;
-                    case 8:
-                        //PRINT command
-                        //print();
-
-                        isValidCommand = true;
-
-                        break;
-                    case 9:
-                        //QUIT command
-                        //quit();
-
-                        isValidCommand = true;
-
-                        break;
-                    default:
-                        break;
-
-                }
+                    break;
+                default:
+                    break;
 
             }
-
-        }
-
-        if (!isValidCommand) {
-
-            getUserInput(userInput);
-            parseUserInput(userInput, commandParameters);
 
         }
 
@@ -173,9 +130,10 @@ void validateUserCommand(string commandParameters[], string validCommands[], str
 }
 
 int main() {
+
+    bool keepGoing = true;
     string validCommands[10] = {"STORE", "RANDOMIZE", "MIN", "MAX", "SUM", "AVE", "VAR", "STD", "PRINT", "QUIT"};
-    string userInput;
-    string commandParameters[4] = {};
+    string usersCommandInput;
 
     // Initialize spreadsheet
     float data[ROWS][COLS];
@@ -187,16 +145,14 @@ int main() {
     data[1][3] = 42;
     print(data, 0, 0, 5, 5);
 
-    // ADD CODE TO HANDLE COMMANDS HERE
-    getUserInput(userInput);
-    parseUserInput(userInput, commandParameters);
+    while (keepGoing) {
 
-    int size = sizeof(commandParameters) / sizeof(commandParameters[0]);
-    for (int i = 0; i < size; i++) {
-        cout << commandParameters[i] << endl;
+        getUserInput(usersCommandInput);
+        validateUserCommand(usersCommandInput, validCommands, data, keepGoing);
+
     }
 
-    validateUserCommand(commandParameters, validCommands, userInput);
+    print(data, 0, 0, 5, 5);
 
     return 0;
 }
